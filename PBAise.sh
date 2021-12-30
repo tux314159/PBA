4f16cce020c563205077c6b7869443f9567d445f
Isaac <tux314159@gmail.com>
2021-12-30 21:35:54 +0800

    damn

--- PBAise.sh
+++ PBAise.sh
@@ -1,25 +1,38 @@
 #! /usr/bin/env sh
 
-pbarules="pba-balance-rules.yaml,pba-briefing-rules.yaml"
-pbanotif="pba-notifications.yaml"
-pbaweap="pba-weapons-rules.yaml"
-rest="bi-balance-rules.yaml,bi-lobby-rules.yaml,bi-player-rules.yaml,bi-sequences.yaml,bi-weapons.yaml,ERCC21andBCC-rules.yaml,ERCC2-sequences.yaml,harv-flipped_top.shp,pip-skull.shp,ragl-weapons.yaml,ref-anim.shp,ref-bot.shp,ref-top.shp,satellite_initialized_delay2s.aud"
+rules="pba-balance-rules.yaml,pba-briefing-rules.yaml,bi-balance-rules.yaml,bi-lobby-rules.yaml,bi-player-rules.yaml,ERCC21andBCC-rules.yaml"
+notifs="pba-notifications.yaml"
+seqs="bi-sequences.yaml,ERCC2-sequences.yaml" # SEX
+weaps="ragl-weapons.yaml,bi-weapons.yaml,pba-weapons-rules.yaml"
+others="harv-flipped_top.shp,pip-skull.shp,ragl-weapons.yaml,ref-anim.shp,ref-bot.shp,ref-top.shp,satellite_initialized_delay2s.aud"
 
 mkdir new
 
+function updatething {
+    sed -i'.bkp' "s|\($1:.*\)|\1,$2|g" $3
+    grep -q "$1:" $3 || printf "\n$1: $2\n" >> $3
+}
+
 for d in *; do
     mapfile=new/$d/map.yaml
+
     if [ ! -d $d ]; then continue; fi
     cp -R $d new
-    for f in $(sh -c "echo diffs/{$(echo $pbarules),$(echo $pbaweap),$(echo $pbanotif),$(echo $rest)}"); do
+    for f in $(sh -c "echo diffs/{$(echo $rules),$(echo $weaps),$(echo $notifs),$(echo $seqs),$(echo $others)}"); do
         cp $f new/$d
     done
+
     (cd new/$d; find -name '*.yaml' -exec dos2unix {} \; 2>/dev/null)
-    sed -i "s|\(Rules:.*\)|\1,$pbarules|g" $mapfile
+    updatething "Rules" $rules $mapfile
+    updatething "Weapons" $weaps $mapfile
+    updatething "Notifications" $notifs $mapfile
+    updatething "Sequences" $seqs $mapfile
     sed -i "s|bi-rules.yaml,||g" $mapfile
-    sed -i "s|\(Weapons:.*\)|\1,$pbaweap|g" $mapfile
-    sed -i "s|\(Notifications:.*\)|\1$pbanotif|g" $mapfile # SPECIAL CASE! NO COMMA
-    grep -q "Notifications:" $mapfile || printf "\nNotifications: $pbanotif\n" >> $mapfile
+
+    # damn
+    #sed -i "s|\(Notifications:.*\)|\1$pbanotif|g" $mapfile # SPECIAL CASE! NO COMMA
+    #grep -q "Notifications:" $mapfile || printf "\nNotifications: $pbanotif\n" >> $mapfile
+
     sed -i "s|\(Title:.*\)\[.*\]|\1[PBA]|g" $mapfile
     sed -i "s|\(Categories:.*\)|Categories: PBA|g" $mapfile
     (cd new/$d; zip -r ../$d.oramap . >/dev/null)
@@ -29,6 +42,7 @@ mkdir sub
 mv new/*.oramap sub
 rm -rf new
 
+find . -name '*.bkp' -print -delete
 rm -f sub.zip
 zip -r sub.zip sub >/dev/null
 rm -rf sub
