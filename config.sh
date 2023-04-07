bcd94d60e9d35b10a1f2bc5a402cb5ddf082c143
Gustas <gustas.kazukauskas@gmail.com>
2023-04-07 11:57:30 +0300

    Update BI

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 # all variables may be left as ""
 # rules, notifs, seqs, weaps, and assets should be comma-seperated
 
-rules="pba-balance-rules.yaml,briefing-rules.yaml,bi-balance-rules.yaml,bi-lobby-rules.yaml,bi-player-rules.yaml,ERCC21andBCC-rules.yaml"
-notifs="pba-notifications.yaml"
-seqs="bi-sequences.yaml,ERCC2-sequences.yaml"
-weaps="ragl-weapons.yaml,bi-weapons.yaml,pba-weapons-rules.yaml"
-assets="harv-flipped_top.shp,pip-skull.shp,ragl-weapons.yaml,ref-anim.shp,ref-bot.shp,ref-top.shp,satellite_initialized_delay2s.aud"
+rules="briefing-rules.yaml,bi-rules-ERCC21andBCC11.yaml,bi-rules-balance.yaml,bi-rules-lobby.yaml"
+notifs="bi-notifications.yaml"
+seqs="bi-sequences-ERCC2.yaml,bi-sequences.yaml"
+weaps=""
+assets="gpssactiveicon.shp,harv-flipped_top.shp,pip-skull.shp,ref-anim.shp,ref-bot.shp,ref-top.shp,satellite_initialized_delay2s.aud,satellite_ready.aud,satellite_terminated.aud"
 
 # text to append to end of map title
 titleappend="[PBA]"
 # overlay image to composite on map preview
-previewoverlay="pbaoverlay.png"
+previewoverlay="overlay.png"
 # map category
 category="PBA"
 
